d282c0feab91a820f5e962de41369b60cd59ebfe
anrogo <antoniorogo96@gmail.com>
2021-11-05 20:31:10 +0100

    login correcto

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImagesComponent } from './images/images.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    ImagesComponent
+    ImagesComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
     NgbModule, // Empaquetado el carrusel
-    FormsModule // Los controles de formulario
+    FormsModule, // Los controles de formulario
+    ReactiveFormsModule
   ]
 })
 export class ComponentsModule { }
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Login } from 'src/app/models/login.model';
+import { LoginService } from 'src/app/services/login.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  providers: [LoginService]
+})
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup;
+  enviado: boolean = false;
+  spinner: boolean = false;
+  errorMsg!: string | null;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private _loginService: LoginService,
+  ) { 
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  // Ejecuta la acción de login
+  submitForm(): void {
+    this.enviado = true;
+    // Si no es válido, no hago nada
+    if(!this.loginForm.valid) {
+      return;
+    } else {
+      //Activo el spinner de cargando:
+      this.spinner = true;
+      //Y si es válido llamamos al servicio de login:
+      this._loginService.performLogin(
+        new Login(
+          this.loginForm.controls.username.value,
+          this.loginForm.controls.password.value,
+          ''))
+      .subscribe(
+        respuesta => {
+        console.log(JSON.stringify(respuesta));
+        this.errorMsg = null;
+        this.spinner = false;
+      },
+      error => {
+        console.log(JSON.stringify(error));
+        this.errorMsg = `⚠️ ¡No se ha podido iniciar la sesión! ${error?.error?.error}`;
+        this.spinner = false;
+      });
+    }
+  }
+}
